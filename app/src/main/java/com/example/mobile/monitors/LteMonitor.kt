c766a1171cff147f88528dd263267c613bc8ada3
Daniele Tarek Iaisy <daniele.tarek.iaisy@hotmail.it>
2023-11-14 16:22:41 +0100

    gestione versioni

--- app/src/main/java/com/example/mobile/monitors/LteMonitor.kt
+++ app/src/main/java/com/example/mobile/monitors/LteMonitor.kt
@@ -1,43 +1,69 @@
 package com.example.mobile.monitors
 
 import android.content.Context
+import android.os.Build
 import android.telephony.PhoneStateListener
 import android.telephony.SignalStrength
+import android.telephony.TelephonyCallback
 import android.telephony.TelephonyManager
+import android.telephony.TelephonyCallback.SignalStrengthsListener
 
 class LteMonitor(
     context: Context
 ): IMonitor {
     private val telephonyManager: TelephonyManager =
         context.getSystemService(Context.TELEPHONY_SERVICE) as TelephonyManager
-    private var signalStrengthListener: PhoneStateListener? = null
     private var signalDbm: Double = 0.0
 
+    private var context = context
+    private var signalStrengthListener: PhoneStateListener? = null
+    private val telephonyCallback = object : TelephonyCallback(), SignalStrengthsListener {
+        override fun onSignalStrengthsChanged(signalStrength: SignalStrength) {
+            // Calculate signal strength value (example: LTE signal strength)
+            signalDbm = computeDbm(signalStrength)
+        }
+    }
+
     //TODO: properly implement this function
-    private fun computeDbm(baseValue: Double): Double {
+    private fun computeDbm(signalStrength: SignalStrength?): Double {
+        var baseValue = signalStrength?.level?.toDouble() ?: 0.0
         return baseValue * 4.0
     }
 
     override fun startMonitoring(onStart: () -> Unit) {
-        signalStrengthListener = object : PhoneStateListener() {
-            override fun onSignalStrengthsChanged(signalStrength: SignalStrength?) {
-                super.onSignalStrengthsChanged(signalStrength)
-                // Calculate signal strength value (example: LTE signal strength)
-                this@LteMonitor.signalDbm =
-                    computeDbm(signalStrength?.level?.toDouble() ?: 0.0)
+        // da Build.VERSION_CODES.S TelephonyManager#listen diventa deprecata e
+        // si usa TelephonyManager#registerTelephonyCallback
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            telephonyManager.registerTelephonyCallback(
+                context.mainExecutor,
+                telephonyCallback
+            )
+        } else {
+            signalStrengthListener = object : PhoneStateListener() {
+                override fun onSignalStrengthsChanged(signalStrength: SignalStrength?) {
+                    super.onSignalStrengthsChanged(signalStrength)
+                    // Calculate signal strength value (example: LTE signal strength)
+                    this@LteMonitor.signalDbm = computeDbm(signalStrength)
+                }
             }
+            telephonyManager.listen(signalStrengthListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS)
+
         }
-        telephonyManager.listen(signalStrengthListener, PhoneStateListener.LISTEN_SIGNAL_STRENGTHS)
-        onStart.invoke()
+        onStart()
     }
 
     override fun stopMonitoring() {
-        signalStrengthListener?.let {
-            telephonyManager.listen(it, PhoneStateListener.LISTEN_NONE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            telephonyManager.unregisterTelephonyCallback(telephonyCallback)
+        } else {
+            signalStrengthListener?.let {
+                telephonyManager.listen(it, PhoneStateListener.LISTEN_NONE)
+            }
         }
     }
 
     override fun readValue(): Double {
+        //TODO: implement db storing
         return signalDbm
     }
 }
