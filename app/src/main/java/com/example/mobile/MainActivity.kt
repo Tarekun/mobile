671cbb66100270b89bb39cf1cb336a0b65fa8784
Daniele Tarek Iaisy <daniele.tarek.iaisy@hotmail.it>
2024-01-03 14:07:13 +0100

    added select button for monitor selection

--- app/src/main/java/com/example/mobile/MainActivity.kt
+++ app/src/main/java/com/example/mobile/MainActivity.kt
@@ -4,16 +4,28 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.mobile.composables.ButtonVariant
+import com.example.mobile.composables.OptionSelect
 import com.example.mobile.composables.ParametrizedButton
 import com.example.mobile.monitors.AudioMonitor
 import com.example.mobile.monitors.LteMonitor
@@ -21,6 +33,8 @@ import com.example.mobile.monitors.MonitorVariant
 import com.example.mobile.monitors.WifiMonitor
 import com.example.mobile.screens.MonitoringScreen
 import com.example.mobile.ui.theme.MobileTheme
+
+
 
 class MainActivity : ComponentActivity() {
     private val audioMonitor by lazy {
@@ -50,17 +64,12 @@ class MainActivity : ComponentActivity() {
                                 Text(text = "")
                             },
                             actions = {
-                                monitors.forEach { monitor ->
-                                    ParametrizedButton(
-                                        //TODO: add icons for buttons
-                                        onClick = {
-                                            inUseMonitor = monitor
-                                        },
-                                        text = monitor.toString(),
-                                        variant = if (inUseMonitor == monitor) ButtonVariant.FILLED else ButtonVariant.TONAL,
-                                        modifier = Modifier,
-                                    )
-                                }
+                                OptionSelect(
+                                    label = "Select Monitor",
+                                    options = monitors,
+                                    onChange = { inUseMonitor = it },
+                                    defaultOption = MonitorVariant.AUDIO
+                                )
                             }
                         )
                     },
@@ -70,7 +79,8 @@ class MainActivity : ComponentActivity() {
                 ) {
                         innerPadding ->
                     Box(
-                        modifier = Modifier.padding(innerPadding)
+                        modifier = Modifier
+                            .padding(innerPadding)
                     ) {
                         when (inUseMonitor) {
                             MonitorVariant.AUDIO -> MonitoringScreen(context = this@MainActivity, audioMonitor)
--- app/src/main/java/com/example/mobile/screens/MonitoringScreen.kt
+++ app/src/main/java/com/example/mobile/screens/MonitoringScreen.kt
@@ -7,12 +7,17 @@ import android.util.Log
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresPermission
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.core.app.ActivityCompat
 import com.example.mobile.composables.Content
 import com.example.mobile.database.DbManager
@@ -122,14 +127,20 @@ fun MonitoringScreen(
         periodMs = newPeriod
     }
 
-    Content(
-        currentVolume = value,
-        start = {
-            startMonitoring()
-        },
-        stop = {
-            stopMonitoring()
-        },
-        onPeriodUpdate = { updateMonitoringPeriod(it) }
-    )
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp)
+    ) {
+        Content(
+            currentVolume = value,
+            start = {
+                startMonitoring()
+            },
+            stop = {
+                stopMonitoring()
+            },
+            onPeriodUpdate = { updateMonitoringPeriod(it) }
+        )
+    }
 }
